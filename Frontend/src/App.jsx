e8a5c1e7de8ad9d09ecb651de642ef6156d59955
shubham <130356788+shubham36-it@users.noreply.github.com>
2025-02-22 19:20:45 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('https://code-reviewer-backend-a6m2.onrender.com', { code })
+    const response = await axios.post('https://code-reviewer-backend-a6m2.onrender.com/ai/get-review', { code })
     setReview(response.data)
   }
 
